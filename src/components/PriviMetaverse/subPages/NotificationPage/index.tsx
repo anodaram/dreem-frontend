0dc948ac6aed8e24cf9620ea5f91e1cef81ed9c9
yuan <react003@protonmail.com>
2022-01-20 03:29:56 +0800

    fix 391 - add padding on notif page

--- src/components/PriviMetaverse/subPages/NotificationPage/index.tsx
+++ src/components/PriviMetaverse/subPages/NotificationPage/index.tsx
@@ -23,13 +23,15 @@ export default function NotificationPage() {
     <Box className={classes.content}>
       <Box className={classes.background}>
         <Box className={classes.title}>Notifications</Box>
-        {notifications.length > 0 ? (
-          notifications.map(n => (
-            <NotificationItem notification={n} removeNotification={removeNotification} />
-          ))
-        ) : (
-          <Box>No unread notifications</Box>
-        )}
+        <Box className={classes.container}>
+          {notifications.length > 0 ? (
+            notifications.map(n => (
+              <NotificationItem notification={n} removeNotification={removeNotification} />
+            ))
+          ) : (
+            <Box>No unread notifications</Box>
+          )}
+        </Box>
       </Box>
     </Box>
   );
@@ -68,13 +70,13 @@ const NotificationItem = ({ notification, removeNotification }) => {
         </NotificationMessage>
         <NotificationButtons
           notification={notification}
-          onDismissNotification={() => {}}
-          refreshAllProfile={() => {}}
-          viewMore={() => {}}
-          setSelectedNotification={() => {}}
-          handleShowContributionModal={() => {}}
-          handleClosePopper={() => {}}
-          handleHidePopper={() => {}}
+          onDismissNotification={() => { }}
+          refreshAllProfile={() => { }}
+          viewMore={() => { }}
+          setSelectedNotification={() => { }}
+          handleShowContributionModal={() => { }}
+          handleClosePopper={() => { }}
+          handleHidePopper={() => { }}
         />
       </ContentContainer>
       <Hidden smDown>
