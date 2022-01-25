e4079ce0e6bd3ab1a08be0205d5a342a8037004b
nicolas <nclszhang92@gmail.com>
2022-01-25 14:38:25 -0700

    implment nft tab in edit draft flow

---
+++ src/components/PriviMetaverse/modals/EditDraftContentModal/components/EditCollectionDraft/index.tsx
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+
+import Box from "shared/ui-kit/Box";
+import { useModalStyles } from "./index.styles";
+
+const EditCollectionDraft = () => {
+  const classes = useModalStyles();
+
+  return <Box></Box>;
+};
+
+export default EditCollectionDraft;
---
+++ src/components/PriviMetaverse/modals/EditDraftContentModal/components/EditFilesDraft/index.tsx
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+
+import Box from "shared/ui-kit/Box";
+import { useModalStyles } from "./index.styles";
+
+const EditFilesDraft = () => {
+  const classes = useModalStyles();
+
+  return <Box></Box>;
+};
+
+export default EditFilesDraft;
