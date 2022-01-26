078d66ee89377c7ebadd9456055398ecb04ff1ac
asatorres <angelsatorres@gmail.com>
2022-01-26 02:37:54 +0100

    fix reserve

--- src/shared/services/API/web3/contracts/reserveMarketplace.ts
+++ src/shared/services/API/web3/contracts/reserveMarketplace.ts
@@ -31,21 +31,37 @@ const reserveMarketplace = (network: string) => {
           )
           .estimateGas({ from: account });
 
-        const response = await contract.methods
-          .cancelPurchaseReserveProposal(
-            payload.collection_id,
-            payload.token_id,
-            payload.paymentToken,
-            payload.collateralToken,
-            payload.price,
-            payload.collateralPercent,
-            payload.reservePeriod,
-            payload.buyer,
-          )
-          .send({ from: account, gas: gas, maxPriorityFeePerGas: web3.utils.toWei(MAX_PRIO_FEE, 'gwei') })
-          .on("transactionHash", hash => {
-            setHash(hash);
-          });
+        const response = await web3.eth.getChainId()==137 
+            ? await contract.methods
+                    .cancelPurchaseReserveProposal(
+                      payload.collection_id,
+                      payload.token_id,
+                      payload.paymentToken,
+                      payload.collateralToken,
+                      payload.price,
+                      payload.collateralPercent,
+                      payload.reservePeriod,
+                      payload.buyer,
+                    )
+                    .send({ from: account, gas: gas, maxPriorityFeePerGas: web3.utils.toWei(MAX_PRIO_FEE, 'gwei') })
+                    .on("transactionHash", hash => {
+                      setHash(hash);
+                    })
+             : await contract.methods
+                    .cancelPurchaseReserveProposal(
+                      payload.collection_id,
+                      payload.token_id,
+                      payload.paymentToken,
+                      payload.collateralToken,
+                      payload.price,
+                      payload.collateralPercent,
+                      payload.reservePeriod,
+                      payload.buyer,
+                    )
+                    .send({ from: account, gas: gas })
+                    .on("transactionHash", hash => {
+                      setHash(hash);
+                    })
 
         console.log("transaction succeed... ", response.events);
         if (response?.events?.PurchaseReserveProposalCanceled) {
@@ -83,21 +99,39 @@ const reserveMarketplace = (network: string) => {
           )
           .estimateGas({ from: account });
 
-        const response = await contract.methods
-          .cancelSaleReserveProposal(
-            payload.collection_id,
-            payload.token_id,
-            payload.paymentToken,
-            payload.collateralToken,
-            payload.price,
-            payload.collateralPercent,
-            payload.reservePeriod,
-            payload.owner,
-          )
-          .send({ from: account, gas: gas, maxPriorityFeePerGas: web3.utils.toWei(MAX_PRIO_FEE, 'gwei') })
-          .on("transactionHash", hash => {
-            setHash(hash);
-          });
+        const response = await web3.eth.getChainId()==137 
+            ? await contract.methods
+                .cancelSaleReserveProposal(
+                  payload.collection_id,
+                  payload.token_id,
+                  payload.paymentToken,
+                  payload.collateralToken,
+                  payload.price,
+                  payload.collateralPercent,
+                  payload.reservePeriod,
+                  payload.owner,
+                )
+                .send({ from: account, gas: gas, maxPriorityFeePerGas: web3.utils.toWei(MAX_PRIO_FEE, 'gwei') })
+                .on("transactionHash", hash => {
+                  setHash(hash);
+                })
+            : await contract.methods
+                  .cancelSaleReserveProposal(
+                    payload.collection_id,
+                    payload.token_id,
+                    payload.paymentToken,
+                    payload.collateralToken,
+                    payload.price,
+                    payload.collateralPercent,
+                    payload.reservePeriod,
+                    payload.owner,
+                  )
+                  .send({ from: account, gas: gas })
+                  .on("transactionHash", hash => {
+                    setHash(hash);
+                  });
+
+
 
         console.log("transaction succeed... ", response?.events);
         if (response?.events?.SaleReserveProposalCanceled) {
@@ -139,24 +173,45 @@ const reserveMarketplace = (network: string) => {
           )
           .estimateGas({ from: account });
         console.log("calced gas price is.... ", gas);
-        const response = await contract.methods
-          .approveReserveToBuy(
-            payload.collection_id,
-            payload.token_id,
-            payload.paymentToken,
-            payload.collateralToken,
-            payload.price,
-            payload.beneficiary,
-            payload.collateralPercent,
-            payload.collateralInitialAmount,
-            payload.reservePeriod,
-            payload.validityPeriod,
-            payload.sellerToMatch
-          )
-          .send({ from: account, gas, maxPriorityFeePerGas: web3.utils.toWei(MAX_PRIO_FEE, 'gwei') })
-          .on("transactionHash", hash => {
-            setHash(hash);
-          });
+
+
+        const response = await web3.eth.getChainId()==137 
+           ? await contract.methods
+                .approveReserveToBuy(
+                  payload.collection_id,
+                  payload.token_id,
+                  payload.paymentToken,
+                  payload.collateralToken,
+                  payload.price,
+                  payload.beneficiary,
+                  payload.collateralPercent,
+                  payload.collateralInitialAmount,
+                  payload.reservePeriod,
+                  payload.validityPeriod,
+                  payload.sellerToMatch
+                )
+                .send({ from: account, gas, maxPriorityFeePerGas: web3.utils.toWei(MAX_PRIO_FEE, 'gwei') })
+                .on("transactionHash", hash => {
+                  setHash(hash);
+                })
+          : await contract.methods
+                .approveReserveToBuy(
+                  payload.collection_id,
+                  payload.token_id,
+                  payload.paymentToken,
+                  payload.collateralToken,
+                  payload.price,
+                  payload.beneficiary,
+                  payload.collateralPercent,
+                  payload.collateralInitialAmount,
+                  payload.reservePeriod,
+                  payload.validityPeriod,
+                  payload.sellerToMatch
+                )
+                .send({ from: account, gas })
+                .on("transactionHash", hash => {
+                  setHash(hash);
+                })
         console.log(response?.events)
         if (
           payload.mode === 0 && response?.events?.PurchaseReserved ||       // Buyer accepts owner's blocking offer
@@ -203,23 +258,43 @@ const reserveMarketplace = (network: string) => {
           )
           .estimateGas({ from: account });
         console.log("calced gas price is.... ", gas);
-        const response = await contract.methods
-          .approveReserveToSell(
-            payload.collection_id,
-            payload.token_id,
-            payload.paymentToken,
-            payload.collateralToken,
-            payload.price,
-            payload.beneficiary,
-            payload.collateralPercent,
-            payload.reservePeriod,
-            payload.validityPeriod,
-            payload.buyerToMatch
-          )
-          .send({ from: account, gas: gas, maxPriorityFeePerGas: web3.utils.toWei(MAX_PRIO_FEE, 'gwei') })
-          .on("transactionHash", hash => {
-            setHash(hash);
-          });
+
+
+        const response = await web3.eth.getChainId()==137 
+            ? await contract.methods
+                .approveReserveToSell(
+                  payload.collection_id,
+                  payload.token_id,
+                  payload.paymentToken,
+                  payload.collateralToken,
+                  payload.price,
+                  payload.beneficiary,
+                  payload.collateralPercent,
+                  payload.reservePeriod,
+                  payload.validityPeriod,
+                  payload.buyerToMatch
+                )
+                .send({ from: account, gas: gas, maxPriorityFeePerGas: web3.utils.toWei(MAX_PRIO_FEE, 'gwei') })
+                .on("transactionHash", hash => {
+                  setHash(hash);
+                })
+            : await contract.methods
+                  .approveReserveToSell(
+                    payload.collection_id,
+                    payload.token_id,
+                    payload.paymentToken,
+                    payload.collateralToken,
+                    payload.price,
+                    payload.beneficiary,
+                    payload.collateralPercent,
+                    payload.reservePeriod,
+                    payload.validityPeriod,
+                    payload.buyerToMatch
+                  )
+                  .send({ from: account, gas: gas })
+                  .on("transactionHash", hash => {
+                    setHash(hash);
+                  })
         console.log("transaction succeed", response.events);
         const offer = response?.events?.SaleReserveProposed || response?.events?.SaleReserved;
         if (
