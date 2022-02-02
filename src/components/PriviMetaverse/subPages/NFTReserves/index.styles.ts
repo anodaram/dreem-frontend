91f70336a755153bd400f6b77510a01f4b55491b
davidli108 <david.li.dev211@gmail.com>
2022-02-02 08:27:57 -0500

    Updated home page

--- src/components/PriviMetaverse/subPages/NFTReserves/index.styles.ts
+++ src/components/PriviMetaverse/subPages/NFTReserves/index.styles.ts
@@ -24,24 +24,6 @@ export const useNFTOptionsStyles = makeStyles(theme => ({
     width: "100%",
     height: "100%",
   },
-  cardOptionButton: {
-    float: "right",
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
-    padding: "4px 8px 2px",
-    background: "rgba(190, 167, 255, 0.6)",
-    borderRadius: "5.90529px",
-    flex: "none",
-    order: 1,
-    flexGrow: 0,
-    color: "#212121",
-    margin: "2px",
-    fontFamily: "GRIFTER",
-    fontStyle: "10px",
-    fontWeight: "bold",
-    fontSize: "8.26741px",
-  },
   content: {
     width: "100%",
     height: "100%",
@@ -54,7 +36,6 @@ export const useNFTOptionsStyles = makeStyles(theme => ({
     overflowX: "hidden",
     position: "relative",
     padding: "160px 32px 45px",
-    maxWidth: 1400,
     marginLeft: "auto",
     marginRight: "auto",
     "& > div > h2": {
@@ -172,258 +153,6 @@ export const useNFTOptionsStyles = makeStyles(theme => ({
       marginRight: '13px'
     }
   },
-  subTitleSection: {
-    display: "flex",
-    width: "100%",
-    fontSize: 18,
-    fontWeight: 800,
-    fontFamily: "Agrandir",
-    color: "#431AB7",
-    lineHeight: "23px",
-    marginTop: 32,
-    padding: "0 20px",
-    cursor: "pointer",
-    [theme.breakpoints.down(1110)]: {
-      fontSize: 15,
-    },
-    [theme.breakpoints.down(950)]: {
-      fontSize: 12,
-    },
-    [theme.breakpoints.down("xs")]: {
-      padding: "0 0",
-    },
-  },
-  optionSection: {
-    display: "flex",
-    alignItems: "center",
-    [theme.breakpoints.down("xs")]: {
-      width: "100%",
-      justifyContent: "flex-end",
-    },
-  },
-  controlBox: {
-    display: "flex",
-    alignItems: "center",
-    background: "#FFFFFF1A",
-    borderRadius: 69,
-  },
-  showButton: {
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
-    border: "none !important",
-    backgroundColor: "transparent !important",
-    minWidth: "unset !important",
-    padding: "10px !important",
-    "& svg": {
-      fill: "white",
-    },
-  },
-  showButtonSelected: {
-    background: "#ffffff !important",
-    boxShadow: "0px 1px 2px rgba(0, 0, 0, 0.08), 0px -1px 20px rgba(0, 0, 0, 0.05)",
-    borderRadius: 34,
-    "& svg": {
-      fill: "#212121",
-    },
-  },
-  listViewRow: {
-    display: "flex",
-    alignItems: "center",
-    fontSize: "14px",
-    fontFamily: "Montserrat",
-    fontWeight: 600,
-    color: "#4218B5",
-    [theme.breakpoints.down("sm")]: {
-      fontSize: "11px",
-    },
-    [theme.breakpoints.down("xs")]: {
-      fontSize: "8px",
-    },
-  },
-  tabSection: {
-    height: 55,
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-    textAlign: "center",
-    margin: "0 40px",
-    fontSize: 18,
-    fontFamily: "Agrandir GrandHeavy",
-    [theme.breakpoints.down(1250)]: {
-      minWidth: 420,
-    },
-    [theme.breakpoints.down(1110)]: {
-      minWidth: 350,
-    },
-    [theme.breakpoints.down(950)]: {
-      minWidth: 275,
-      fontSize: 14,
-    },
-    [theme.breakpoints.down(580)]: {
-      minWidth: 165,
-      fontSize: 16,
-      margin: "0 0",
-      padding: "0 24px",
-      height: "84px",
-      width: "50%",
-    },
-    borderBottom: "4px solid transparent",
-  },
-  selectedTabSection: {
-    borderBottom: "4px solid #431AB7",
-  },
-  explorerContent: {
-    padding: "35px 0",
-    width: "100%",
-  },
-  filterTag: {
-    background: "linear-gradient(301.58deg, #ED7B7B 32.37%, #EDFF1C 100.47%)",
-    borderRadius: "100px",
-    marginTop: "15px",
-    marginRight: "10px",
-    color: "white",
-    fontFamily: "Rany",
-    fontWeight: 400,
-    fontSize: 12,
-    lineHeight: "18px",
-    padding: "8px 14px",
-    cursor: "pointer",
-  },
-  filterActive: {
-    color: "#fff",
-    "& *": {
-      color: "#fff !important",
-    },
-    "& svg path": {
-      fill: "#fff !important",
-    },
-  },
-  filterButtonBox: {
-    background: "#FFFFFF1A",
-    display: "flex",
-    alignItems: "center",
-    padding: `${theme.spacing(1)}px`,
-    cursor: "pointer",
-    borderRadius: "100vh",
-    color: "white",
-    [theme.breakpoints.down("xs")]: {
-      padding: "8px",
-    },
-  },
-  searchOpened: {
-    padding: "inherit 24px",
-  },
-  select: {
-    marginTop: "8px",
-    background: "#212121",
-    padding: "8px 12px",
-    "& + &": {
-      marginLeft: 10,
-    },
-    "& .MuiSelect-root": {
-      padding: "0px 4px",
-      fontFamily: "Rany",
-      fontWeight: 700,
-      fontSize: 14,
-      color: "rgba(255, 255, 255, 0.5)",
-      marginRight: 8,
-      "&:focus": {
-        backgroundColor: "unset",
-      },
-      "& svg": {
-        width: 18,
-        height: 18,
-      },
-      "& span": {
-        fontSize: 14,
-        fontWeight: 700,
-        color: "#E9FF26",
-      },
-    },
-    "& svg path": {
-      stroke: "#E9FF26",
-    },
-  },
-  chainImage: {
-    width: "20px",
-    marginRight: "8px",
-  },
-  table: {
-    minHeight: 400,
-    "& .MuiTable-root": {
-      borderSpacing: "0px 8px",
-      borderCollapse: "unset",
-    },
-    "& .MuiTableCell-root": {
-      border: "none",
-      fontFamily: "Agrandir",
-    },
-    "& .MuiTableRow-head": {
-      background: "transparent",
-      "& .MuiTableCell-head": {
-        border: "none",
-        color: "#4218B5",
-        fontSize: "14px",
-        fontFamily: "Montserrat",
-        fontWeight: 600,
-        [theme.breakpoints.down("sm")]: {
-          fontSize: "12px",
-        },
-      },
-    },
-    "& .MuiTableBody-root": {
-      "& .MuiTableCell-body": {
-        fontSize: 14,
-        fontWeight: 600,
-        fontFamily: "Montserrat",
-        color: "white",
-        [theme.breakpoints.down("sm")]: {
-          fontSize: 12,
-        },
-        [theme.breakpoints.down("xs")]: {
-          fontSize: 8,
-        },
-      },
-      "& .MuiTableRow-root": {
-        cursor: "pointer",
-        background: "#151515 !important",
-        "& td:first-child": {
-          borderTopLeftRadius: "14px",
-          borderBottomLeftRadius: "14px",
-        },
-        "& td:last-child": {
-          borderTopRightRadius: "14px",
-          borderBottomRightRadius: "14px",
-        },
-      },
-    },
-
-    [theme.breakpoints.down("sm")]: {
-      minHeight: 200,
-    },
-  },
-  listNFTImage: {
-    height: 60,
-    width: 60,
-    "& img": {
-      width: "100%",
-      borderRadius: 4,
-    },
-  },
-  listLoading: {
-    display: "flex",
-    alignItems: "center",
-    width: "100%",
-    border: "2px solid #F2C525",
-    borderRadius: 16,
-    padding: 12,
-    background: "rgba(255, 255, 255, 0.1) !important",
-    "& .MuiSkeleton-root": {
-      backgroundColor: "#505050",
-      borderRadius: 6,
-    },
-  },
   gameslider: {
     border: "2px solid transparent",
     borderImageSource: "linear-gradient(180deg, rgba(255, 255, 255, 0) 42%, #E9FF26 100%)",
@@ -445,71 +174,152 @@ export const useNFTOptionsStyles = makeStyles(theme => ({
   sliderLeft: {
     position: 'absolute',
     bottom: '100px',
-    left: '-56px'
+    left: '-110px'
   },
   sliderRight: {
     position: 'absolute',
     top: '90px',
-    right: '-65px'
+    right: '-110px'
   },
   sliderRect: {
     position: 'absolute',
     bottom: 0,
     right: '-59px'
-  }
-}));
-
-export const useFilterSelectStyles = makeStyles({
-  paper: {
-    marginTop: "10px",
-    marginLeft: "-13px",
-    background: "#212121",
-    boxShadow: "0px 15px 35px -31px rgba(13, 12, 62, 0.19)",
-    color: "rgba(255, 255, 255, 0.5)",
-    "& svg": {
-      width: 18,
-      height: 18,
-      marginRight: 8,
+  },
+  NFTSection: {
+    width: "100%",
+    marginTop: 46,
+  },
+  topGamesWrapper: {
+    paddingTop: "50px",
+    [theme.breakpoints.down(1200)]: {
+      paddingTop: "30px",
     },
   },
-  list: {
-    padding: "20px 8px!important",
-    paddingRight: 8,
-    "& .MuiListItem-root": {
-      marginBottom: 10,
-      padding: "2px 8px",
-      minWidth: "200px",
-      Height: "36px",
-      "&:last-child": {
-        marginBottom: 0,
-      },
-      "&:hover": {
-        color: "white",
-        border: "solid 1px #E9FF26",
+  topGamesTitle: {
+    display: "flex",
+    alignItems: "center",
+    paddingBottom: "25px",
+    "& span": {
+      fontFamily: "GRIFTER",
+      fontSize: 34,
+      fontWeight: 700,
+      background: "linear-gradient(#B7FF5C, #EEFF21)",
+      "-webkit-text-fill-color": "transparent",
+      "-webkit-background-clip": "text",
+      lineHeight: "40.8px",
+      [theme.breakpoints.down("xs")]: {
+        fontSize: 25,
       },
     },
-  }
-});
-
-export const useTabsStyles = makeStyles(theme => ({
-  tab: {
-    fontSize: "16px",
-    fontWeight: 700,
-    lineHeight: "19.2px",
-    color: "white",
-    borderRadius: "2px",
-    padding: "8px 20px",
-    textTransform: "uppercase",
+    "& img": {
+      width: 80,
+      height: 80,
+    },
+  },
+  carouselNav: {
     cursor: "pointer",
-    [theme.breakpoints.down("xs")]: {
-      flex: 1,
-      fontSize: 10,
-      padding: "8px 10px",
-      textAlign: "center",
+    border: "2px solid rgba(255, 255, 255, 0.5)",
+    borderRadius: "8px",
+    width: 74,
+    height: 42,
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    "&:hover": {
+      border: "2px solid #E9FF26",
+      "& path": {
+        stroke: "#E9FF26",
+        opacity: 1
+      },
     },
   },
-  selected: {
-    color: "#212121",
-    background: "linear-gradient(92.31deg, #EEFF21 -2.9%, #B7FF5C 113.47%)",
+  topNFTContent: {
+    display: "flex",
+    background: "transparent",
+    paddingBottom: 20,
+    position: "relative",
+    [theme.breakpoints.down(769)]: {
+      marginBottom: 0,
+      paddingBottom: 0,
+    },
+    "& button.rec-arrow-left": {
+      position: "absolute",
+      top: -100,
+      right: 200,
+
+      [theme.breakpoints.down(1200)]: {
+        right: 60,
+      },
+      [theme.breakpoints.down(769)]: {
+        // display: "none",
+      },
+    },
+    "& button.rec-arrow-right": {
+      position: "absolute",
+      top: -100,
+      right: 140,
+
+      [theme.breakpoints.down(1200)]: {
+        right: 0,
+      },
+      [theme.breakpoints.down(769)]: {
+        display: "none",
+      },
+    },
+    "& .rec-slider-container": {
+      margin: "0px -12px",
+      width: "calc(100% + 24px)",
+      "& .rec-carousel-item": {
+        "& > .rec-item-wrapper": {
+          // minWidth: 330,
+          // maxWidth: 370,
+          // width: "unset",
+          "& > div": {
+            boxShadow: "none",
+          },
+        },
+      },
+    },
+  },
+  allNFTSection: {
+    width: "100%",
+    marginBottom: 40,
+  },
+  allNFTWrapper: {
+    padding: "50px 40px",
+    [theme.breakpoints.down(860)]: {
+      padding: "50px 25px",
+    },
+  },
+  fitContent: {
+    maxWidth: 1600,
+    marginLeft: "auto",
+    marginRight: "auto",
+    paddingLeft: "32px !important",
+    paddingRight: "32px !important",
+    [theme.breakpoints.down("sm")]: {
+      paddingLeft: "16px !important",
+      paddingRight: "16px !important",
+    },
+  },
+  allNFTTitle: {
+    display: "flex",
+    alignItems: "center",
+    fontSize: 24,
+    fontWeight: 800,
+    color: "#431AB7",
+    fontFamily: "Agrandir GrandHeavy",
+    lineHeight: "130%",
+    "& span": {
+      paddingBottom: 25,
+    },
+    "& img": {
+      width: 80,
+      height: 80,
+    },
+    [theme.breakpoints.down("xs")]: {
+      fontSize: 25,
+    },
   },
 }));
