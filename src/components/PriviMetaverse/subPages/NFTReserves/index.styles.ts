dd7987aa06e0a36d5b945d470a43af4886a68545
nicolas <nclszhang92@gmail.com>
2022-02-08 10:22:20 -0700

    fix responsive issues on maketplace main page

--- src/components/PriviMetaverse/subPages/NFTReserves/index.styles.ts
+++ src/components/PriviMetaverse/subPages/NFTReserves/index.styles.ts
@@ -225,10 +225,11 @@ export const useNFTOptionsStyles = makeStyles(theme => ({
     },
     [theme.breakpoints.down("sm")]: {
       maxWidth: 480,
-      height: 330,
+      maxHeight: 550,
     },
     [theme.breakpoints.down("xs")]: {
       maxWidth: 350,
+      minHeight: 400,
     },
   },
   gameContent: {
@@ -243,6 +244,9 @@ export const useNFTOptionsStyles = makeStyles(theme => ({
     [theme.breakpoints.down("sm")]: {
       padding: "18px",
       justifyContent: "flex-start",
+    },
+    [theme.breakpoints.down("xs")]: {
+      padding: "12px",
     },
   },
   gameBgImage: {
@@ -512,8 +516,18 @@ export const useNFTOptionsStyles = makeStyles(theme => ({
       fontFamily: "Rany",
       opacity: 0.6,
     },
+    [theme.breakpoints.down("sm")]: {
+      padding: "0px 48px",
+    },
     [theme.breakpoints.down("xs")]: {
-      padding: 0,
+      padding: "0px 24px",
+      alignItems: "center",
+      "& span:first-child": {
+        fontSize: 24,
+      },
+      "& span:last-child": {
+        fontSize: 12,
+      },
     },
   },
   listLoading: {
