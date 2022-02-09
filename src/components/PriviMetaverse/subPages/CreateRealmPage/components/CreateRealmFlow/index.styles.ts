514864ae61d408c82ae5650229384fc2f9226ab1
volodymyrboian <volodmyr@protonmail.com>
2022-02-09 12:00:36 -0800

    fix cutting modal, bad overlays

--- src/components/PriviMetaverse/subPages/CreateRealmPage/components/CreateRealmFlow/index.styles.ts
+++ src/components/PriviMetaverse/subPages/CreateRealmPage/components/CreateRealmFlow/index.styles.ts
@@ -7,7 +7,7 @@ export const useModalStyles = makeStyles(theme => ({
     color: "white",
     maxWidth: "755px",
     borderRadius: "0px",
-    maxHeight: "576px",
+    maxHeight: "calc(100vh - 370px)",
     overflow: "auto",
   },
   headTitle: {
